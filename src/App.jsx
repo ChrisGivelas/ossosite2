96efd65dff88b9e486dcce9bdfd3593456857b92
ChrisGivelas <chris_g_50@hotmail.com>
2019-11-04 14:45:46 -0500

    use jsx extension for App component + remove auth widget

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import {Switch, Route} from "react-router-dom";
 import Main from "./pages/main";
-import {Shared} from "./components";
 
 function App() {
     return (
         <div id="app">
-            <Shared.AuthWidget />
             <Switch>
                 <Route path="/" component={Main}></Route>
             </Switch>
