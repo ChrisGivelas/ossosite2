76c90db72a1e4177d62335ed3e0e779b42a27c64
Christopher Givelas <chris_g_50@hotmail.com>
2019-11-07 12:59:08 -0500

    Updates

---
+++ precache-manifest.28be33480fedf9689d6e7c1d327863d8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9273fa7c44143a9cd92afee8e6beb805",
+    "url": "/ossosite2/index.html"
+  },
+  {
+    "revision": "a7184f5043bf63436489",
+    "url": "/ossosite2/static/css/main.90ca95f0.chunk.css"
+  },
+  {
+    "revision": "30c4e8f708bb6d2ec9ae",
+    "url": "/ossosite2/static/js/2.826b77a1.chunk.js"
+  },
+  {
+    "revision": "a7184f5043bf63436489",
+    "url": "/ossosite2/static/js/main.89b7ecd0.chunk.js"
+  },
+  {
+    "revision": "97c5c1498de5a977e246",
+    "url": "/ossosite2/static/js/runtime-main.30d815cb.js"
+  },
+  {
+    "revision": "85d57ab90b0b6e0e2c71add6ffb1159f",
+    "url": "/ossosite2/static/media/Jomolhari-Regular.85d57ab9.ttf"
+  },
+  {
+    "revision": "f9baef8ac0d836e6486419e282e42336",
+    "url": "/ossosite2/static/media/Quicksand-VariableFont:wght.f9baef8a.ttf"
+  },
+  {
+    "revision": "0bc96bb839e70a7134d241322545fa7e",
+    "url": "/ossosite2/static/media/[H] Osso Lighting Logo 2018 (blue).0bc96bb8.png"
+  },
+  {
+    "revision": "4762dbefa002b93dd856e7fa4aab9051",
+    "url": "/ossosite2/static/media/landing1.4762dbef.jpg"
+  },
+  {
+    "revision": "a5751bd8737355c903fd065d892e3e8c",
+    "url": "/ossosite2/static/media/parallax1.a5751bd8.jpg"
+  }
+]);
