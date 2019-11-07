9e47fa34eb275be2d3cd6ee9fe1e9c3695caa39e
Christopher Givelas <chris_g_50@hotmail.com>
2019-11-07 12:29:06 -0500

    Add contact form and styling improvements

--- src/components/unique/Contact.jsx
+++ src/components/unique/Contact.jsx
@@ -7,6 +7,13 @@ function Contact() {
             <section id="contact-section">
                 <h3 className="section-title">Contact Us</h3>
                 <p className="section-description">Description goes here</p>
+                <form id="contact-form">
+                    <input type="text" placeholder="Name" />
+                    <input type="text" placeholder="Email" />
+                    <input type="text" placeholder="Subject" />
+                    <textarea placeholder="Message" />
+                    <input type="submit" value="submit" />
+                </form>
             </section>
         </ScrollableAnchor>
     );
