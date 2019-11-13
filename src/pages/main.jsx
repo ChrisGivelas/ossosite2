cd0cf5520ede0932cd703c99a5ed871091ce720e
Christopher Givelas <chris_g_50@hotmail.com>
2019-11-13 16:51:37 -0500

    Change parallax images and change style of navlinks on hover

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {Unique} from "../components";
-import * as Parallax from '../assets/images/parallax1.jpg'
+import * as Parallax from "../assets/images/parallax1.jpg";
 
 const {Landing, Suppliers, About, Contact} = Unique;
 
@@ -8,11 +8,11 @@ function Main() {
     return (
         <div id="main">
             <Landing />
-            <div className="parallax-divider parallax-img-1" src={Parallax}/>
+            <div className="parallax-divider parallax-img-1" src={Parallax} />
             <Suppliers />
-            <div className="parallax-divider parallax-img-1" src={Parallax}/>
+            <div className="parallax-divider parallax-img-2" src={Parallax} />
             <About />
-            <div className="parallax-divider parallax-img-1" src={Parallax}/>
+            <div className="parallax-divider parallax-img-3" src={Parallax} />
             <Contact />
         </div>
     );
