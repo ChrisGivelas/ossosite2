b77fcfcaf7d7091f4930b9d3b78dfc80cd9e6137
Christopher Givelas <chris_g_50@hotmail.com>
2019-11-15 13:39:12 -0500

    Temporarily add product_images to media included in bundle so that we can deploy to github pages

--- src/components/unique/Suppliers.jsx
+++ src/components/unique/Suppliers.jsx
@@ -3,9 +3,42 @@ import ScrollableAnchor from "react-scrollable-anchor";
 import {getImageURL} from "../../utils";
 import {Shared} from "../";
 
+import Artcraft0 from "../../assets/images/supplier_products/Artcraft/0.jpg";
+import Artcraft1 from "../../assets/images/supplier_products/Artcraft/1.jpg";
+import Artcraft2 from "../../assets/images/supplier_products/Artcraft/2.jpg";
+import Artcraft3 from "../../assets/images/supplier_products/Artcraft/3.jpg";
+import Artcraft4 from "../../assets/images/supplier_products/Artcraft/4.jpg";
+import Artcraft5 from "../../assets/images/supplier_products/Artcraft/5.jpg";
+import Artcraft6 from "../../assets/images/supplier_products/Artcraft/6.jpg";
+import Artcraft7 from "../../assets/images/supplier_products/Artcraft/7.jpg";
+
 const {Carousel, ProductImage} = Shared;
 
 const suppliers = require("../../suppliers.json");
+
+// var supplierProducts = (function() {
+//     const numbOfProducts = 8;
+
+//     var products = [];
+
+//     for (var i = 0; i < numbOfProducts; i++) {
+//         products.push(<ProductImage src={getImageURL(`${i}.jpg`, "images/supplier_products/Artcraft")} />);
+//     }
+
+//     return products;
+// })();
+
+//Temporarily do it this way just so that we can deploy to github pages
+var supplierProducts = [
+    <ProductImage src={Artcraft0} />,
+    <ProductImage src={Artcraft1} />,
+    <ProductImage src={Artcraft2} />,
+    <ProductImage src={Artcraft3} />,
+    <ProductImage src={Artcraft4} />,
+    <ProductImage src={Artcraft5} />,
+    <ProductImage src={Artcraft6} />,
+    <ProductImage src={Artcraft7} />
+];
 
 function Supplier({id, name, website, description, productTypes, items}) {
     const selfRef = useRef();
@@ -57,18 +90,6 @@ function Supplier({id, name, website, description, productTypes, items}) {
         </div>
     );
 }
-
-const supplierProducts = (function() {
-    const numbOfProducts = 8;
-
-    var products = [];
-
-    for (var i = 0; i < numbOfProducts; i++) {
-        products.push(<ProductImage src={getImageURL(`${i}.jpg`, "images/supplier_products/Artcraft")} />);
-    }
-
-    return products;
-})();
 
 function Suppliers() {
     return (
