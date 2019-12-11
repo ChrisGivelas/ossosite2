b2da25d0eaf7e66ca91d7935da44a38fac4aab90
Christopher Givelas <chris_g_50@hotmail.com>
2019-12-11 16:51:10 -0500

    2 Different layouts for about section carousel

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,9 +1,26 @@
-import React, {useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import ScrollableAnchor from "react-scrollable-anchor";
-import Modal from "react-bootstrap/Modal";
 import Carousel from "../shared/Carousel";
 
 import MarinoOsso from "../../assets/images/Marino Osso.jpg";
+
+var aboutText = [
+    `Since its inception in 1984, Osso City Lighting has proudly been one of the Durham Region’s largest
+    purveyors of fine lighting. Over a quarter of a century later, Osso City Lighting has expanded its
+    horizons to include home accents and accent furniture. We work closely with suppliers to provide you`,
+    `with only the highest quality items to satisfy all of your lighting and décor needs. Marino Osso,
+    long-time owner of Osso City Lighting, first arrived in Canada 1966, at the age of nineteen. Being
+    raised by two hardworking farmers inspired Marino to make an honest living, so he decided to take up`,
+    `a trade. After obtaining his license in Electrical Construction Maintenance from George Brown
+    College in 1972, Marino opened a contracting operation called Osso Electric Limited. For the next 12
+    years, Marino worked on perfecting his craft, as well as gaining knowledge of the lighting and décor`,
+    `world. Developing his skills as a lighting expert and businessman gave him the confidence to sell
+    Osso Electric in 1984, and begin a new venture known as Osso City Lighting. Thanks to the support
+    that we have received from the community, Osso City Lighting is now celebrating 30+ years of`,
+    `customer satisfaction. Our goal for the future is to continue to provide the same level of
+    excellence that we have shown since our beginning. Whether its traditional, contemporary, or
+    anything in between, Osso City Lighting has what you need.`
+];
 
 var aboutImages = [
     <img className="about-image" alt="about" src={MarinoOsso} />,
@@ -13,76 +30,74 @@ var aboutImages = [
     <img className="about-image" alt="about" src={MarinoOsso} />
 ];
 
-// var aboutText = [
-//     `Since its inception in 1984, Osso City Lighting has proudly been one of the Durham Region’s largest
-//     purveyors of fine lighting. Over a quarter of a century later, Osso City Lighting has expanded its
-//     horizons to include home accents and accent furniture. We work closely with suppliers to provide you`,
-//     `with only the highest quality items to satisfy all of your lighting and décor needs. Marino Osso,
-//     long-time owner of Osso City Lighting, first arrived in Canada 1966, at the age of nineteen. Being
-//     raised by two hardworking farmers inspired Marino to make an honest living, so he decided to take up`,
-//     `a trade. After obtaining his license in Electrical Construction Maintenance from George Brown
-//     College in 1972, Marino opened a contracting operation called Osso Electric Limited. For the next 12
-//     years, Marino worked on perfecting his craft, as well as gaining knowledge of the lighting and décor`,
-//     `world. Developing his skills as a lighting expert and businessman gave him the confidence to sell
-//     Osso Electric in 1984, and begin a new venture known as Osso City Lighting. Thanks to the support
-//     that we have received from the community, Osso City Lighting is now celebrating 30+ years of`,
-//     `customer satisfaction. Our goal for the future is to continue to provide the same level of
-//     excellence that we have shown since our beginning. Whether its traditional, contemporary, or
-//     anything in between, Osso City Lighting has what you need.`
-// ];
-
-function AboutModal({show, handleHide}) {
-    return (
-        <Modal show={show} onHide={handleHide}>
-            <Modal.Header closeButton>
-                <Modal.Title>Our Story</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <p id="about-content" className="section-content">
-                    Since its inception in 1984, Osso City Lighting has proudly been one of the Durham Region’s largest
-                    purveyors of fine lighting. Over a quarter of a century later, Osso City Lighting has expanded its
-                    horizons to include home accents and accent furniture. We work closely with suppliers to provide you
-                    with only the highest quality items to satisfy all of your lighting and décor needs.
-                    <br />
-                    <br />
-                    Marino Osso, long-time owner of Osso City Lighting, first arrived in Canada 1966, at the age of
-                    nineteen. Being raised by two hardworking farmers inspired Marino to make an honest living, so he
-                    decided to take up a trade. After obtaining his license in Electrical Construction Maintenance from
-                    George Brown College in 1972, Marino opened a contracting operation called Osso Electric Limited.
-                    For the next 12 years, Marino worked on perfecting his craft, as well as gaining knowledge of the
-                    lighting and décor world. Developing his skills as a lighting expert and businessman gave him the
-                    confidence to sell Osso Electric in 1984, and begin a new venture known as Osso City Lighting.
-                    <br />
-                    <br />
-                    Thanks to the support that we have received from the community, Osso City Lighting is now
-                    celebrating 30+ years of customer satisfaction. Our goal for the future is to continue to provide
-                    the same level of excellence that we have shown since our beginning. Whether its traditional,
-                    contemporary, or anything in between, Osso City Lighting has what you need.
-                </p>
-            </Modal.Body>
-        </Modal>
-    );
-}
-
 function About() {
-    const [showModal, setShowModal] = useState(false);
+    const [verticalMode, setVerticalMode] = useState();
+    const selfRef = useRef();
+    const slickRef = React.createRef();
 
-    const handleShowModal = () => setShowModal(true);
-    const handleHideModal = () => setShowModal(false);
+    const updateMode = _ => {
+        if (verticalMode && window.innerWidth >= 650) {
+            setVerticalMode(false);
+        } else if (!verticalMode && window.innerWidth < 650) {
+            setVerticalMode(true);
+        }
+    };
+
+    useEffect(() => {
+        if (!window.onresize) {
+            updateMode();
+            window.onresize = function() {
+                updateMode();
+            };
+        }
+
+        if (!selfRef.current.onmouseover) {
+            selfRef.current.onmouseover = function(e) {
+                slickRef.current && slickRef.current.slickPause();
+                e.stopPropagation();
+            };
+        }
+
+        if (!selfRef.current.onmouseout) {
+            selfRef.current.onmouseout = function(e) {
+                if (slickRef.current && slickRef.current.props && slickRef.current.props.autoplay) {
+                    slickRef.current && slickRef.current.slickPlay();
+                    e.stopPropagation();
+                }
+            };
+        }
+
+        return _ => {
+            window.onresize = undefined;
+        };
+    }, [verticalMode]);
 
     return (
         <React.Fragment>
             <ScrollableAnchor id="about">
-                <section id="about-section" onClick={handleShowModal}>
-                    <div id="view-about-overlay" style={window.isTouchEnabled ? {opacity: 0.5} : {}}>
-                        <Carousel autoplaySpeed={4000}>{aboutImages}</Carousel>
+                <section id="about-section">
+                    <h3 className="section-title">Our Story</h3>
+                    <hr />
+                    <div id="about-content-container" ref={selfRef}>
+                        <Carousel
+                            slickRef={slickRef}
+                            autoplaySpeed={4000}
+                            speed={1000}
+                            variableWidth={!verticalMode}
+                            autoplay={false}
+                            centerMode
+                            arrows
+                            infinite={false}
+                            vertical={verticalMode}
+                            verticalSwiping={verticalMode}
+                            draggable={verticalMode}
+                        >
+                            {aboutImages}
+                        </Carousel>
                     </div>
-                    <div id="view-about-button-container" style={window.isTouchEnabled ? {opacity: 1} : {}}>
-                        <h5>Our Story</h5>
-                    </div>
+                    <hr />
                 </section>
             </ScrollableAnchor>
-            <AboutModal show={showModal} handleHide={handleHideModal} />
         </React.Fragment>
     );
 }
--- src/components/shared/Carousel.jsx
+++ src/components/shared/Carousel.jsx
@@ -18,11 +18,13 @@ export default class Carousel extends React.Component {
         autoplaySpeed: 2000,
         pauseOnHover: false,
         draggable: false,
+        accessibility: true,
         slickRef: undefined
     };
 
     render() {
         const {slickRef, children, ...settings} = this.props;
+        console.log("rerender with props:", this.props);
 
         return (
             <Slider ref={slickRef} {...settings}>
