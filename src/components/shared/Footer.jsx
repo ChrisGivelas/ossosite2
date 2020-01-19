6bdcbd2b030a546c41e6e10a313a8bcd53df9066
Christopher Givelas <chris_g_50@hotmail.com>
2020-01-19 13:39:11 -0500

    Remove unnecessary html wrapping for social media icons

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -6,23 +6,25 @@ export default function Footer() {
         <div id="footer">
             <p id="footer-content">&copy;2019 by Osso City Lighting. All Rights Reserved.</p>
             <div id="social-media">
-                <ul>
-                    <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/OssoCityLighting/">
-                        <li className="fab fa-facebook-f"></li>
-                    </a>
-                    <Tippy content="Coming soon!">
-                        <a>
-                            <li className="fab fa-instagram"></li>
-                        </a>
-                    </Tippy>
-                    <a
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        href="https://www.youtube.com/channel/UCni1-Y1rMqU4lHdy0eoJj4g"
-                    >
-                        <li className="fab fa-youtube"></li>
-                    </a>
-                </ul>
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://www.facebook.com/OssoCityLighting/"
+                    className="fab fa-facebook-f"
+                >
+                    <React.Fragment />
+                </a>
+                <Tippy content="Coming soon!">
+                    <span className="fab fa-instagram"></span>
+                </Tippy>
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://www.youtube.com/channel/UCni1-Y1rMqU4lHdy0eoJj4g"
+                    className="fab fa-youtube"
+                >
+                    <React.Fragment />
+                </a>
             </div>
         </div>
     );
