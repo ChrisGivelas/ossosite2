07b1ce1eb25ede189317a23c616c1f798b4d21fc
Christopher Givelas <christopher.givelas@jonahgroup.com>
2020-01-21 17:03:48 -0500

    Landing page text change

--- src/components/sections/Landing.jsx
+++ src/components/sections/Landing.jsx
@@ -7,9 +7,7 @@ function Landing() {
             <div id="landing-content-background">
                 <div id="landing-content-container">
                     <img id="logo" src={Logo} alt="Osso City Lighting" />
-                    <h1 style={{color: "white"}}>
-                        Bringing You Fine Lighting and Décor with Over 30+ Years Of Experience.
-                    </h1>
+                    <h1 style={{color: "white"}}>Bringing You Fine Lighting and Décor for Over 30+ Years.</h1>
                 </div>
             </div>
         </section>
