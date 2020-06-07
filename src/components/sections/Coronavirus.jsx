87afb94694c8786a4ab61cc5e196a8446737516b
Chris Givelas <chris_g_50@hotmail.com>
2020-06-07 04:05:36 -0400

    Add Coronavirus Section

---
+++ src/components/sections/Coronavirus.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Coronavirus() {
+  return (
+    <section id="coronavirus">
+      <h1 className="section-title">Coronavirus Update</h1>
+      <h4 className="section-description">Coronavirus information here!</h4>
+    </section>
+  );
+}
+
+export default Coronavirus;
--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -4,21 +4,23 @@ import Landing from "../components/sections/Landing";
 import Suppliers from "../components/sections/Suppliers";
 import About from "../components/sections/About";
 import Contact from "../components/sections/Contact";
+import Coronavirus from "../components/sections/Coronavirus";
 
 import Footer from "../components/shared/Footer";
 
 function Main() {
-    return (
-        <div id="main">
-            <Landing />
-            <div id="main-content">
-                <Suppliers />
-                <About />
-                <Contact />
-            </div>
-            <Footer />
-        </div>
-    );
+  return (
+    <div id="main">
+      <Landing />
+      <div id="main-content">
+        <Coronavirus />
+        <Suppliers />
+        <About />
+        <Contact />
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default Main;
