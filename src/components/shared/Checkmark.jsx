d71407f4b8c95000a0e718fd5a80344a33ce8c22
Chris Givelas <chris_g_50@hotmail.com>
2020-06-08 02:08:46 -0400

    autoformat

--- src/components/shared/Checkmark.jsx
+++ src/components/shared/Checkmark.jsx
@@ -1,8 +1,14 @@
 import React from "react";
-import CheckmarkIcon from "../../assets/images/checkmark.png";
+import CheckmarkIcon from "../../assets/images/CheckMark.png";
 
 function Spinner() {
-    return <img style={{marginLeft: 15, maxWidth: 20, maxHeight: 20}} src={CheckmarkIcon} alt="checkmark" />;
+  return (
+    <img
+      style={{ marginLeft: 15, maxWidth: 20, maxHeight: 20 }}
+      src={CheckmarkIcon}
+      alt="checkmark"
+    />
+  );
 }
 
 export default Spinner;
