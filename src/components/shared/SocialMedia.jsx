1f0b65cb9326ca2081b87483c60f2ec4ab9473d0
Chris Givelas <chris_g_50@hotmail.com>
2021-11-26 10:43:17 -0500

    Replace social media buttons

--- src/components/shared/SocialMedia.jsx
+++ src/components/shared/SocialMedia.jsx
@@ -1,26 +1,25 @@
 import React from "react";
+import { Youtube, Facebook } from "../../assets/images/IconSvgs";
 
 function SocialMedia() {
-    return (
-        <div id="social-media">
-            <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://www.facebook.com/OssoCityLighting/"
-                className="fab fa-facebook-f"
-            >
-                <React.Fragment />
-            </a>
-            <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://www.youtube.com/channel/UCni1-Y1rMqU4lHdy0eoJj4g"
-                className="fab fa-youtube"
-            >
-                <React.Fragment />
-            </a>
-        </div>
-    );
+  return (
+    <div id="social-media">
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.facebook.com/OssoCityLighting/"
+      >
+        <Youtube />
+      </a>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.youtube.com/channel/UCni1-Y1rMqU4lHdy0eoJj4g"
+      >
+        <Facebook />
+      </a>
+    </div>
+  );
 }
 
 export default SocialMedia;
